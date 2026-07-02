52d9ca827e5a4af92c4024ebca55d6f283209801
Swathi Suresh MK <97884284+Swathi-22@users.noreply.github.com>
2026-07-02 21:57:37 +0530

    Add files via upload

---
+++ Student_Grade_Management_System.py
@@ -0,0 +1,29 @@
+
+def calculate_average(marks):
+    toatl = 0
+    for i in marks:
+        toatl += int(i)
+    avg = toatl / len(marks)
+    return avg
+    
+
+def get_grade(average):
+    if average >= 90:
+        grade = "A"
+    elif average >= 80 or average < 90:
+        grade = "B"
+    elif average >= 70 or average < 80:
+        grade = "C"
+    elif average >= 60 or average < 70:
+        grade = "D"
+    elif average < 60:
+        grade = "F"
+    return grade
+
+
+marks = input("Enter marks for 5 subjects : ")
+mrk_splt = marks.split()
+average = calculate_average(mrk_splt)
+grade = get_grade(average)
+print(f"Average Marks: {average}")
+print(f"Grade: {grade}")
